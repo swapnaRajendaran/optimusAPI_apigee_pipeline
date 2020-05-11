3e8a4e00198614614a53fffea7cd27042a56fcb0
swapnaRajendaran <61879148+swapnaRajendaran@users.noreply.github.com>
2020-05-11 13:53:18 -0500

    Update deploy.sh

--- infrastructure/deploy.sh
+++ infrastructure/deploy.sh
@@ -1,1 +1,6 @@
-
+aws cloudformation deploy \
+    --stack-name=a202210-codepipeline-tutorial \
+    --template-file=./infrastructure/codepipeline.yaml \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameter-overrides $(cat ./infrastructure/pipelineParameters.properties) \
+    --tags tr:application-asset-insight-id=202209 tr:resource-owner=swapna.rajendaran@thomsonreuters.com
